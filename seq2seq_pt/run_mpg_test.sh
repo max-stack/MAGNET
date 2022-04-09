94cad340565475c197b66f8a5e1b1c61f819977f
max-stack <55951090+max-stack@users.noreply.github.com>
2022-04-09 10:38:48 +0100

    Changing values and dataset locations

--- seq2seq_pt/run_mpg_test.sh
+++ seq2seq_pt/run_mpg_test.sh
@@ -2,8 +2,8 @@
 
 set -x
 
-DATAHOME=/path/to/MPG/math_problem_generation/dolphint1_eval
-EXEHOME=/path/to/MPG/seq2seq_pt
+DATAHOME=$(dirname "${PWD}")/data
+EXEHOME=$PWD
 
 SAVEPATH=${DATAHOME}/models/magnet
 
@@ -12,8 +12,10 @@ mkdir -p ${SAVEPATH}
 cd ${EXEHOME}
 
 python translate.py \
-       -model ${SAVEPATH}/model_dev_metric_0.1241_e14.pt \
-       -src ${DATAHOME}/dev/dev.equ \
+       -model ${SAVEPATH}/model_e20.pt \
+       -src ${DATAHOME}/test/dev.equ.txt \
+       -tgt ${DATAHOME}/test/dev.nl.txt \
+       -lda ${DATAHOME}/test/dev.lda.txt \
        -batch_size 1 -beam_size 3 \
        -gpu 0 \
-       -output /tmp/mpg.dev.s2s.batch1.beam3.out
+       -output ${DATAHOME}/test/output.txt
