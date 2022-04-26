78d4a5de80a997affe807e5e3a389847e1e279bb
max-stack <55951090+max-stack@users.noreply.github.com>
2022-04-26 18:47:03 +0100

    Changes to the run configuration

--- seq2seq_pt/run_mpg_test.sh
+++ seq2seq_pt/run_mpg_test.sh
@@ -5,17 +5,17 @@ set -x
 DATAHOME=$(dirname "${PWD}")/data
 EXEHOME=$PWD
 
-SAVEPATH=${DATAHOME}/models/magnet
+SAVEPATH="${DATAHOME}/models/magnet"
 
-mkdir -p ${SAVEPATH}
+mkdir -p "${SAVEPATH}"
 
 cd ${EXEHOME}
 
 python translate.py \
-       -model ${SAVEPATH}/model_e20.pt \
-       -src ${DATAHOME}/test/dev.equ.txt \
-       -tgt ${DATAHOME}/test/dev.nl.txt \
-       -lda ${DATAHOME}/test/dev.lda.txt \
+       -model "${SAVEPATH}/model_e20.pt" \
+       -src "${DATAHOME}/test/dev.equ.txt" \
+       -tgt "${DATAHOME}/test/dev.nl.txt" \
+       -lda "${DATAHOME}/test/dev.lda.txt" \
        -batch_size 1 -beam_size 3 \
-       -gpu 0 \
-       -output ${DATAHOME}/test/output.txt
+       -gpu -1 \
+       -output "${DATAHOME}/test/output.txt"
